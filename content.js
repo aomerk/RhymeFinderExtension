72eaa43550034a3a5e7a40775735c0459d36a40f
aliomerkeser <aliomerkeser@gmail.com>
2020-10-04 20:25:28 +0000

    remove content.js

--- content.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({'url': chrome.extension.getURL('rhyme.html')}, function(tab) {
-    // open rhyme finder
-  });
-});
