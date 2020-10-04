8f5187a0925f5b67b0132c633aea01a625c63875
aliomerkeser <aliomerkeser@gmail.com>
2020-10-04 20:25:16 +0000

    open page on click

--- background.js
+++ background.js
@@ -8,3 +8,9 @@
  * *****
  *  *****
  * */
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({'url': chrome.extension.getURL('rhyme.html')}, function(tab) {
+    // open rhyme finder
+  });
+});
